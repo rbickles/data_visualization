c124b77665a8661e0ff4497edc74baeeb3a1e2b9
rbickles <74263176+rbickles@users.noreply.github.com>
2025-02-11 16:45:26 -0500

    enhance basketball visuals.

--- src/basketball/basketball.js
+++ src/basketball/basketball.js
@@ -1,4 +1,6 @@
 import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 export async function renderBasketball(container) {
   const scene = new THREE.Scene();
@@ -8,46 +10,53 @@ export async function renderBasketball(container) {
     0.1,
     1000
   );
-
   const renderer = new THREE.WebGLRenderer();
+
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  const textureLoader = new THREE.TextureLoader();
-  const ballTexture = textureLoader.load("src/images/basketball.jpg");
-  ballTexture.repeat.set(5, 5); // Increases tiling, making dimples smaller
-  ballTexture.wrapS = THREE.MirroredRepeatWrapping; // Reflects texture instead of hard reset
-  ballTexture.wrapT = THREE.MirroredRepeatWrapping;
-  ballTexture.minFilter = THREE.LinearFilter; // Smoothens small details
-  ballTexture.magFilter = THREE.LinearMipMapLinearFilter;
+  let basketball;
 
-  const geometry = new THREE.SphereGeometry(4, 32, 32);
+  // Load GLTF Model
+  const loader = new GLTFLoader();
+  loader.load(
+    "src/images/scene.gltf",
+    function (gltf) {
+      basketball = gltf.scene;
+      scene.add(gltf.scene);
+    },
+    undefined,
+    function (error) {
+      console.error(error);
+    }
+  );
 
-  const material = new THREE.MeshStandardMaterial({
-    map: ballTexture,
-    roughness: 0.8,
-    metalness: 0.1,
-  });
+  // Camera positioning
+  camera.position.x = 5;
+  camera.position.y = 2;
+  camera.position.z = 5;
 
-  const basketball = new THREE.Mesh(geometry, material);
-  scene.add(basketball);
+  // Orbit controls
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
 
-  const directional_light = new THREE.DirectionalLight(0xffffff, 1);
-  directional_light.position.set(-5, 10, 5);
-  directional_light.castShadow = true;
-  scene.add(directional_light);
+  const light = new THREE.PointLight(0xffffff, 100, 100);
+  light.position.set(5, 5, 5);
+  scene.add(light);
 
-  const ambientLight = new THREE.AmbientLight(0xffffff, 2);
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
   scene.add(ambientLight);
 
-
-
-  camera.position.z = 10;
-  const animate = () => {
+  // Animation loop
+  function animate() {
     requestAnimationFrame(animate);
-
-    basketball.rotation.y += 0.01;
+    if (basketball) {
+      //basketball.rotation.x +=0.005;
+      basketball.rotation.y += 0.005;
+    }
+    controls.update();
     renderer.render(scene, camera);
-  };
+  }
+
   animate();
 }
